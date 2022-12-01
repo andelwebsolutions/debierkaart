b10baebafa5fce1e5b0845311547f6c63d906efa
andelwebsolutions <info@andelwebsolutions.nl>
2022-12-01 12:36:32 +0100

    App skeleton and webapp package installation

---
+++ assets/Pages/App.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from "react";
+
+const App: FC = () => {
+    return (
+        <>
+            <h1>Debierkaart</h1>
+        </>
+    );
+};
+
+export default App;
--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,8 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./pages/App";
 
-// any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import "./bootstrap";
 
-// start the Stimulus application
-import './bootstrap';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App/>);
