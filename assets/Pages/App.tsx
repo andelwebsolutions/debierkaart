9e593e3d7df13711a65ebfa579444635f9082bd3
andelwebsolutions <info@andelwebsolutions.nl>
2022-12-01 20:26:57 +0100

    Progress on front-end

--- assets/Pages/App.tsx
+++ assets/Pages/App.tsx
@@ -1,10 +1,80 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+import { Box, Heading, Text, Input, InputGroup, InputLeftElement, Button, Flex, Image, FormControl, FormErrorMessage } from "@chakra-ui/react";
+import { SearchIcon } from "@chakra-ui/icons";
+import axios from "axios";
+import { css } from "@emotion/react";
 
 const App: FC = () => {
+    const [breweries, setBreweries] = useState([]);
+    const [isLoading, setIsLoading] = useState<Boolean>(0);
+    const [query, setQuery] = useState({
+        zipcode: ''
+    });
+    const [errors, setErrors] = useState([]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        setIsLoading(1);
+        setBreweries([]);
+
+        const queryString = Object.keys(query)
+            .map(k => encodeURIComponent(k) + '=' + encodeURIComponent(query[k]))
+            .join("&");
+
+        axios.get("/api/search?" + queryString)
+            .then((response) => {
+                setBreweries(response.data);
+            }).catch(error => {
+                if (error.response.status != 422) return;
+
+                setErrors(error.response.data);
+            }).finally(() => setIsLoading(0));
+    };
+
     return (
-        <>
-            <h1>Debierkaart</h1>
-        </>
+        <ChakraProvider>
+            <Box css={css`
+                    background: rgb(250, 204, 21);
+                    background-size: contain;
+                    background: -webkit-linear-gradient(rgba(17, 24, 39, 0.2), rgba(17, 24, 39, 0.8)), url("/background.jpg");
+                    background: linear-gradient(rgba(17, 24, 39, 0.2), rgba(17, 24, 39, 0.8)), url("/background.jpg");`}
+                 bgPosition="center" bgRepeat="no-repeat">
+                <Box maxW="960px" mx="auto" pt="48" pb="28" px="4">
+                    <Flex>
+                        <Image w="8" src="/logo.png"/>
+                        <Heading ml="2" textColor="white" fontSize="3xl">De Bierkaart</Heading>
+                    </Flex>
+                    <Text textColor="gray.100" mt="4" fontSize="lg">Zoek een bierbrouwerij bij jou in de buurt!</Text>
+
+                    <Box mt="4">
+                        <form method="POST" onSubmit={handleSubmit}>
+                            <InputGroup gap='2'>
+                                <InputLeftElement
+                                    pointerEvents='none'
+                                    children={<SearchIcon color='gray.300' />}
+                                />
+                                <Input type="text" name="zipcode" placeholder="Vul je postcode in..."
+                                       pl="10"
+                                       required
+                                       color="white"
+                                       _placeholder={{ color: 'gray.200' }}
+                                       onChange={e => setQuery({ ...query, zipcode: e.target.value })}/>
+                                <Button color="gray.50" bgColor="yellow.500" _hover={{ bg: "yellow.400" }} type="submit" isLoading={isLoading === 1} loadingText="Laden...">Zoeken</Button>
+                            </InputGroup>
+                        </form>
+                    </Box>
+                </Box>
+            </Box>
+
+            <Box maxW="960px" mx="auto" py="12">
+                { breweries.data && breweries.data.length
+                    ? breweries.data.map((brewery, breweryIdx) => <Heading>{brewery.name}</Heading>)
+                    : null }
+            </Box>
+
+        </ChakraProvider>
     );
 };
 
